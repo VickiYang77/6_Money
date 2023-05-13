622a260167d3195739b8610e24438738a982f2dd
Vicki <vicki@mitake.com.tw>
2023-05-13 15:23:31 +0800

    init

---
+++ Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by 金融研發一部-楊雅婷 on 2023/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
