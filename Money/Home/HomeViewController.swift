5d74a116ebc680f5f2329d6acf65ff4e50459bdf
Vicki <vicki@mitake.com.tw>
2023-07-01 15:54:37 +0800

    [Update] 將delete方法搬到VM中

--- Money/Home/HomeViewController.swift
+++ Money/Home/HomeViewController.swift
@@ -34,7 +34,7 @@ class HomeViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        viewModel.fetchRecords.send()
+        viewModel.fetchRecords()
     }
     
     private func setupUI() {
@@ -161,16 +161,8 @@ extension HomeViewController: UITableViewDelegate {
             let cellDatas = self.viewModel.getSectionRecords(indexPath.section)
             if cellDatas.count > indexPath.row {
                 let cellInfo = cellDatas[indexPath.row]
-                APIService.share.delete(.record, ids: [cellInfo.id]) {
-                    DispatchQueue.main.async {
-                        self.viewModel.records.removeAll(where: { $0.id == cellInfo.id })
-                        if self.viewModel.getSectionRecords(indexPath.section).count == 0 {
-                            self.viewModel.dateSection.remove(at: indexPath.section)
-                        }
-                        
-                        completionHandler(true)
-                    }
-                }
+                self.viewModel.deleteRecord(cellInfo)
+                completionHandler(true)
             }
         }
         
--- Money/Home/HomeViewModel.swift
+++ Money/Home/HomeViewModel.swift
@@ -14,29 +14,37 @@ class HomeViewModel {
     @Published var budget: Int = 5000
     private var cancellable = Set<AnyCancellable>()
     
-    var fetchRecords = PassthroughSubject<Void, Never>()
-    
     init() {
         setupBinding()
     }
     
     func setupBinding() {
-        fetchRecords
-            .sink {
-                APIService.share.fetch(.record) { [weak self] (response: RecordsModel?) in
-                    guard let self = self, let records = response?.records else { return }
-                    
-                    self.dateSection = Set(records.map(\.fields.date)).sorted(by: >)
-                    self.records = records.sorted {
-                        if $0.fields.date == $1.fields.date {
-                            return $0.fields.updateTime > $1.fields.updateTime
-                        } else {
-                            return $0.fields.date > $1.fields.date
-                        }
-                    }
-                }
+        // 更新section
+        $records
+            .sink { [weak self] (records) in
+                self?.dateSection = Set(records.map(\.fields.date)).sorted(by: >)
             }
             .store(in: &cancellable)
+    }
+    
+    func fetchRecords() {
+        APIService.share.fetch(.record) { [weak self] (response: RecordsModel?) in
+            guard let self = self, let records = response?.records else { return }
+            
+            self.records = records.sorted {
+                if $0.fields.date == $1.fields.date {
+                    return $0.fields.updateTime > $1.fields.updateTime
+                } else {
+                    return $0.fields.date > $1.fields.date
+                }
+            }
+        }
+    }
+    
+    func deleteRecord(_ record: RecordModel) {
+        APIService.share.delete(.record, ids: [record.id]) { [weak self] in
+            self?.records.removeAll(where: { $0.id == record.id })
+        }
     }
     
     func getSectionRecords(_ index: Int) -> [RecordModel] {
